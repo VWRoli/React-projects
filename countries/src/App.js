3b0f499356b3a7225b6a70e574873714a477f7c8
VWRoli <fustroli@gmail.com>
2021-03-02 10:56:16 +0100

    added custom hook to fetch data

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,40 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Countries from './Countries';
 import Loading from './Loading';
 import Error from './Error';
-//import useFetchCountries from './useFetchCountries';
+import { useFetch } from './useFetch';
 
 const BASE_URL = `https://restcountries.eu/rest/v2/all`;
 
 function App() {
-  //Loading state
-  const [isLoading, setIsLoading] = useState(true);
-  //Error state
-  const [isError, setIsError] = useState(false);
+  const { isLoading, isError, data } = useFetch(BASE_URL);
 
-  const [data, setData] = useState([]);
-
-  const fetchCountryData = async () => {
-    try {
-      const response = await fetch(BASE_URL);
-      const data = await response.json();
-      setData(data);
-      //Disable loading screen
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      setIsError(true);
-    }
-  };
-  useEffect(() => {
-    fetchCountryData();
-  }, []);
   //Loading screen
   if (isLoading) {
     return <Loading />;
   }
-
   //Error screen
   if (isError) {
     return <Error />;
