4641294193d6ad62382eb2778dbaf1810f064663
VWRoli <fustroli@gmail.com>
2021-03-04 10:04:22 +0100

    rearranged navbar

--- personal-portfolio/src/components/Navbar.js
+++ personal-portfolio/src/components/Navbar.js
@@ -1,10 +1,48 @@
+import React, { useState, useRef, useEffect } from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll';
-import { FaCode } from 'react-icons/fa';
+import { links } from './navbarData';
+import { FaCode, FaTimes, FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav>
-      <Link
+      <div className="nav-header">
+        <FaCode className="logo-icon" />
+        Roland Füst
+      </div>
+      <div className="nav-container">
+        <ul className="links">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <Link
+                  className="link-items"
+                  to={url}
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  {text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="nav-resume">
+        <button className="resume-btn">
+          <a href="cv_roland_fust.pdf" target="_blank">
+            Resume
+          </a>
+        </button>
+        <button className="nav-toggle">
+          <FaBars />
+        </button>
+      </div>
+      {/*  <Link
         className="navbar-logo"
         to="hero"
         activeClass="active"
@@ -58,7 +96,7 @@ const Navbar = () => {
         <a href="cv_roland_fust.pdf" target="_blank">
           Resume
         </a>
-      </button>
+      </button> */}
     </nav>
   );
 };
---
+++ personal-portfolio/src/components/navbarData.js
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    id: 1,
+    url: 'hero',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: 'projects',
+    text: 'Projects',
+  },
+  {
+    id: 3,
+    url: 'about',
+    text: 'About',
+  },
+];
