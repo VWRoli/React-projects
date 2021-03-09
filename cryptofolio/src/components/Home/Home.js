362b998aa25f39cc5b648e3d1fe2693119e9ed69
VWRoli <fustroli@gmail.com>
2021-03-09 09:01:20 +0100

    finished homepage styling

--- cryptofolio/src/components/Home/Home.js
+++ cryptofolio/src/components/Home/Home.js
@@ -19,16 +19,18 @@ const Home = () => {
   //Default
   return (
     <section id="home">
-      <div className="home-left">
-        <h1>Crypto Portfolio Tracker</h1>
-        <h2>
-          Keep track of your profits, losses and portfolio valuation with our
-          easy to use platform.
-        </h2>
-        <button>Create your Portfolio</button>
-      </div>
-      <div className="home-right">
-        <HomeList coins={data} />
+      <div className="container">
+        <div className="home-left">
+          <h1>Crypto Portfolio Tracker</h1>
+          <h2>
+            Keep track of your profits, losses and portfolio valuation with our
+            easy to use platform.
+          </h2>
+          <button type="button">Create your Portfolio</button>
+        </div>
+        <div className="home-right">
+          <HomeList coins={data} />
+        </div>
       </div>
     </section>
   );
