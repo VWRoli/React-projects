397663697929a2cecd42dc0c11e3d1b680ca8e76
VWRoli <fustroli@gmail.com>
2021-03-14 15:43:23 +0100

    working on the chart section

--- cryptofolio/src/components/Portfolio/Chart.js
+++ cryptofolio/src/components/Portfolio/Chart.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useCallback } from 'react';
 import { priceFormatter } from '../../helpers';
 import {
   XAxis,
@@ -7,15 +8,87 @@ import {
   Tooltip,
   Area,
 } from 'recharts';
-import { useFetch } from '../../useFetch';
+
 import Loading from '../Loading';
 import Error from '../Error';
 
-const graph_url = `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7`;
+const urlArray = [
+  `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7`,
+  `https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=7`,
+  `https://api.coingecko.com/api/v3/coins/litecoin/market_chart?vs_currency=usd&days=7`,
+];
 
 const Chart = () => {
-  //Convert price array into object
-  const { data, isError, isLoading } = useFetch(graph_url);
+  //Loading state
+  const [isLoading, setIsLoading] = useState(false);
+  //Error state
+  const [isError, setIsError] = useState(false);
+
+  const [data, setData] = useState([]);
+
+  //Fetch market chart data
+
+  const fetchAllData = async (urls) => {
+    setIsLoading(true);
+    setIsLoading(false);
+    try {
+      const response = await Promise.all(
+        urls.map((url) => fetch(url).catch((error) => error))
+      );
+
+      //Data conststs price data, market cap data, and total volume data
+      const data = await Promise.all(
+        response.map((response) =>
+          response.json ? response.json().catch((error) => error) : response
+        )
+      );
+
+      //Get prices from data array
+      const priceData = data.map((item) => {
+        return item.prices.slice(0, 169);
+      });
+
+      //Get timestamps for chart
+      const timeStamps = priceData[0].map((stamp) => stamp[0]);
+
+      //Get and add the total price values
+      const totalPrices = priceData
+        .map((array) => array.map((el) => el[1]))
+        .reduce((acc, curr) => acc.map((el, i) => el + curr[i]));
+
+      //Createing a data Object for the chart
+      const chartData = timeStamps.map((el, i) => {
+        //Configuration
+        const options = {
+          hour: 'numeric',
+          minute: 'numeric',
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric',
+          weekday: 'long',
+        };
+
+        //Locale
+        const locale = navigator.language;
+        //Formatting the date
+        const formattedDate = new Intl.DateTimeFormat(locale, options).format(
+          new Date(el)
+        );
+
+        return { day: formattedDate, price: totalPrices[i] };
+      });
+
+      setData(chartData);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+  useEffect(() => {
+    fetchAllData(urlArray);
+  }, []);
+
   //Loading screen
   if (isLoading) {
     return <Loading />;
@@ -26,33 +99,10 @@ const Chart = () => {
     return <Error />;
   }
 
-  const tempGrapghArray = data.prices;
-
-  const graphObj = tempGrapghArray.map((el) => {
-    //Configuration
-    const options = {
-      hour: 'numeric',
-      minute: 'numeric',
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric',
-      weekday: 'long',
-    };
-
-    //Locale
-    const locale = navigator.language;
-    //Formatting the date
-    const formattedDate = new Intl.DateTimeFormat(locale, options).format(
-      new Date(el[0])
-    );
-
-    return { day: formattedDate, price: el[1] };
-  });
-
   return (
     <>
       <ResponsiveContainer width="100%" height={300}>
-        <AreaChart data={graphObj}>
+        <AreaChart data={data}>
           <defs>
             <linearGradient id="priceGradient" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
