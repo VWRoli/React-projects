9d7b1b726da3b3f10f0442d1a15afbcf6a10d754
VWRoli <fustroli@gmail.com>
2021-03-17 16:05:04 +0100

    added refresh button

--- cryptofolio/src/components/Portfolio/Assets.js
+++ cryptofolio/src/components/Portfolio/Assets.js
@@ -1,13 +1,16 @@
 import AssetsHeader from './AssetsHeader';
 import { useGlobalContext } from '../../context';
 import AssetsTable from './AssetsTable';
+import { FaSyncAlt } from 'react-icons/fa';
 
 const Assets = () => {
-  const { assets, clearAssets } = useGlobalContext();
+  const { assets, clearAssets, fetchCoinInfo } = useGlobalContext();
 
   return (
     <section id="assets">
-      <h2>My Assets</h2>
+      <h2 className="assets-title">
+        My Assets <FaSyncAlt className="refresh-btn" onClick={fetchCoinInfo} />
+      </h2>
       <table>
         <thead>
           <AssetsHeader />
--- cryptofolio/src/context.js
+++ cryptofolio/src/context.js
@@ -144,6 +144,7 @@ export const AppProvider = ({ children }) => {
         closeSuccess,
         setSearchQuery,
         setIsError,
+        fetchCoinInfo,
       }}
     >
       {children}
