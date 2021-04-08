b98902fefc9a81becc46c23702909ad350bd977a
VWRoli <fustroli@gmail.com>
2021-04-08 17:20:02 +0200

    init

---
+++ memory-card-game/src/Components/Start.tsx
@@ -0,0 +1,9 @@
+const Start: React.FC = () => {
+  return (
+    <section className="start-page">
+      <button className="btn start-btn">Start Game</button>
+    </section>
+  );
+};
+
+export default Start;
