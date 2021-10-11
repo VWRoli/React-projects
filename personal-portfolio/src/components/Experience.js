9826c17d081fb17dee3ffc5a7b85469ab137b234
VWRoli <fustroli@gmail.com>
2021-10-11 16:04:24 +0200

    style: add style to experience section

--- personal-portfolio/src/components/Experience.js
+++ personal-portfolio/src/components/Experience.js
@@ -1,18 +1,45 @@
+import Tag from './utils/Tag';
+
 const Experience = () => {
   return (
     <section id="experience">
-      <div className="experience-wrapper">
+      <div>
         <h1>Work experience</h1>
-        <h2>Volunteer React developer @Greenstand</h2>
-        <p>Aug 2021 - Present</p>
-        <ul>
-          <li>Main contributor for the treetracker web wallet.</li>
-          <li>
-            Created reusable and scalable frontend components for the app, using
-            React and Material-UI.
-          </li>
-          <li>Helped setting up the project infrastructure.</li>
-        </ul>
+        <div className="experience-content-wrapper">
+          <div className="company-title">
+            <h2>
+              <a
+                href="https://greenstand.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Greenstand
+              </a>
+            </h2>
+          </div>
+          <div>
+            <h2>Volunteer React developer</h2>
+            <p className="tag">Aug 2021 - Present</p>
+            <ul>
+              <li>
+                Main contributor for the{' '}
+                <a
+                  href="https://github.com/Greenstand/treetracker-wallet-web"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  treetracker web wallet
+                </a>
+                .
+              </li>
+              <li>
+                Creating reusable and scalable frontend components for the app,
+                using <Tag type="React" /> and <Tag type="Material-UI" />.
+              </li>
+              <li>Helped setting up the project infrastructure.</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
